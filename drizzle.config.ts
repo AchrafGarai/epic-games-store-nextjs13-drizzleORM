c6b04b6485d22abc5eee7b981d8241ef2db98fbd
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-09-06 15:10:03 +0100

    Init Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    ssl: true,
+  },
+} satisfies Config;
---
+++ src/db/schema.ts
@@ -0,0 +1,35 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  uniqueIndex,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+// declaring enum in database
+export const popularityEnum = pgEnum("popularity", [
+  "unknown",
+  "known",
+  "popular",
+]);
+
+export const countries = pgTable(
+  "countries",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+  },
+  (countries) => {
+    return {
+      nameIndex: uniqueIndex("name_idx").on(countries.name),
+    };
+  }
+);
+
+export const cities = pgTable("cities", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  countryId: integer("country_id").references(() => countries.id),
+  popularity: popularityEnum("popularity"),
+});
