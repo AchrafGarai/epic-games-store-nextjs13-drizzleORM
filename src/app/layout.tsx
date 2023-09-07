6bd7c6cc95e508e5f6e03ba7baf3a4ff8c6a5f98
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-09-07 16:18:45 +0100

    Add dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <div>
-          <UserButton afterSignOutUrl="/" />
+          <UserButton
+            afterSignOutUrl="/"
+            userProfileMode="navigation"
+            userProfileUrl="/user-profile"
+          />
         </div>
         Hello
       </div>
