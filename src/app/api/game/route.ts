c6d65933eae920d353aad6ba80a5eaae15f1c082
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-09-15 15:12:08 +0100

    Added zod validation

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,6 +1,7 @@
-import { db } from '@/db'
-import { NextResponse } from 'next/server'
-import { eq } from 'drizzle-orm'
+import { db } from "@/db";
+import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
 
 export async function GET() {
   const data = await db.query.games.findMany({
@@ -32,13 +33,28 @@ export async function GET() {
         },
       },
     },
-  })
-  // const data = await db
-  //   .select()
-  //   .from(games)
-  //   .leftJoin(platforms, eq(games.id, platforms.platformId))
-  //   .leftJoin(categories, eq(games.id, categories.id));
-  return NextResponse.json({ data })
+  });
+  return NextResponse.json({ data });
 }
 
-export const revalidate = 1
+export async function POST(request: Request) {
+  const schema = z.object({
+    title: z.string(),
+    realeaseDate: z.string(),
+    images: z.array(z.string().url()),
+    categories: z.array(z.string()),
+  });
+
+  const req = await request.json();
+  const r = schema.safeParse(req);
+  if (!r.success) {
+    const { errors } = r.error;
+    return new Response("Invalid Data Format", {
+      status: 400,
+    });
+  }
+  const { title, images, realeaseDate, categories } = req;
+  return NextResponse.json({ title, images, realeaseDate, categories });
+}
+
+export const revalidate = 1;
