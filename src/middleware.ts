a210a30d29d5eee702e2d4b70568a1f7a5a8639a
AchrafGarai <achrafgarai@hotmail.com>
2023-10-22 13:58:57 +0100

    project restructure

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from '@clerk/nextjs'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api(.*)"],
-});
+  // Make all api public
+  // publicRoutes: ["/api(.*)"],
+  // Make only wbhook api public
+  publicRoutes: ['/api/webhooks/(.*)'],
+})
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
